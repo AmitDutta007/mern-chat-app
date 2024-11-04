75f0f3b7c57daa84f6c40f4974be08d20a4a8da1
amitdutta <duttaamit356@gmail.com>
2024-11-04 23:58:13 +0530

    Added signup functionality

--- client/src/components/Authentication/SignUp.jsx
+++ client/src/components/Authentication/SignUp.jsx
@@ -4,6 +4,8 @@ import { Input, InputGroup, InputRightElement } from "@chakra-ui/input";
 import { VStack } from "@chakra-ui/layout";
 import { useToast } from "@chakra-ui/toast";
 import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 
 const SignUp = () => {
@@ -13,8 +15,77 @@ const SignUp = () => {
     const [email, setEmail] = useState();
     const [confirmpassword, setConfirmpassword] = useState();
     const [password, setPassword] = useState();
-    const [pic, setPic] = useState();
-    const [picLoading, setPicLoading] = useState(false);
+    // const [pic, setPic] = useState();
+    // const [picLoading, setPicLoading] = useState(false);
+
+    const toast = useToast();
+    const navigate = useNavigate();
+
+
+    const submitHandler = async () => {
+        // setPicLoading(true);
+        if (!name || !email || !password || !confirmpassword) {
+            toast({
+                title: "Please Fill all the Feilds",
+                status: "warning",
+                duration: 5000,
+                isClosable: true,
+                position: "bottom",
+            });
+            // setPicLoading(false);
+            return;
+        }
+        if (password !== confirmpassword) {
+            toast({
+                title: "Passwords Do Not Match",
+                status: "warning",
+                duration: 5000,
+                isClosable: true,
+                position: "bottom",
+            });
+            return;
+        }
+        console.log(name, email, password);
+        try {
+            const config = {
+                headers: {
+                    "Content-type": "application/json",
+                },
+            };
+            const { data } = await axios.post(
+                "/api/user/register",
+                {
+                    name,
+                    email,
+                    password,
+                },
+                config
+            );
+            console.log(data);
+            toast({
+                title: "Registration Successful",
+                status: "success",
+                duration: 5000,
+                isClosable: true,
+                position: "bottom",
+            });
+            localStorage.setItem("userInfo", JSON.stringify(data));
+            // setPicLoading(false);
+            navigate("/chats");
+        } catch (error) {
+            toast({
+                title: "Error Occured!",
+                description: error.response.data.message || "An error occurred",
+                status: "error",
+                duration: 5000,
+                isClosable: true,
+                position: "bottom",
+            });
+            // setPicLoading(false);
+        }
+    };
+
+
 
     return (
         <VStack spacing="15px">
@@ -56,28 +127,13 @@ const SignUp = () => {
                         placeholder="Confirm password"
                         onChange={(e) => setConfirmpassword(e.target.value)}
                     />
-                    <InputRightElement width="4.5rem">
-                        {/* <Button h="1.75rem" size="sm" onClick={handleClick}>
-                            {show ? "Hide" : "Show"}
-                        </Button> */}
-                    </InputRightElement>
                 </InputGroup>
-            </FormControl>
-            <FormControl id="pic">
-                <FormLabel>Upload your Picture</FormLabel>
-                <Input
-                    type="file"
-                    p={1.5}
-                    accept="image/*"
-                    // onChange={(e) => postDetails(e.target.files[0])}
-                />
             </FormControl>
             <Button
                 colorScheme="blue"
                 width="100%"
                 style={{ marginTop: 15 }}
-                // onClick={submitHandler}
-                isLoading={picLoading}
+                onClick={submitHandler}
             >
                 Sign Up
             </Button>
