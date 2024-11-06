1f613f4172dc26108b54ed6cdfaf9cd022342a8a
amitdutta <duttaamit356@gmail.com>
2024-11-06 17:34:07 +0530

    create fechchats and create group chat api

--- api/routes/chatRoutes.js
+++ api/routes/chatRoutes.js
@@ -1,19 +1,19 @@
 import express from "express"
 import {
     accessChat,
-    // fetchChats,
-    // createGroupChat,
-    // removeFromGroup,
-    // addToGroup,
-    // renameGroup,
+    fetchChats,
+    createGroupChat,
+    removeFromGroup,
+    addToGroup,
+    renameGroup,
 } from '../controllers/chatControllers.js'
 import { protect } from '../middleware/authMiddleware.js'
 
 const router = express.Router();
 
 router.post("/", protect, accessChat);
-// router.post("/", protect, fetchChats);
-// router.post("/group", protect, createGroupChat);
+router.post("/", protect, fetchChats);
+router.post("/group", protect, createGroupChat);
 // router.put("/rename", protect, renameGroup);
 // router.put("/groupremove", protect, removeFromGroup);
 // router.put("/groupadd", protect, addToGroup);
