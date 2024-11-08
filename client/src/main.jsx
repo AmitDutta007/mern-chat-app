a9e5d07282f7aafad5efa08337ed061bd6d5222e
amitdutta <duttaamit356@gmail.com>
2024-11-08 16:06:20 +0530

    Created Chat context

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,12 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { ChakraProvider } from '@chakra-ui/react'
-
+import ChatProvider from './components/Context/ChatProvider.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ChakraProvider>
-      <App />
+      <ChatProvider>
+        <App />
+      </ChatProvider>
     </ChakraProvider>
   </StrictMode>,
 )
